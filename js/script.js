c6e97d3e481116b2de187fe6a4b67f3d4fead625
Rami Alkawadri <ramialkawadri@hotmail.com>
2022-06-28 09:15:41 +0200

    more responsive, fixing nav

--- js/script.js
+++ js/script.js
@@ -1,82 +1,90 @@
-"use strict";
+'use strict';
 
 // Scrolling behavior
-const mainNavlEl = document.querySelector(".main-nav");
-const buttonUpEl = document.querySelector(".top-button");
+const mainNavlEl = document.querySelector('.main-nav');
+const buttonUpEl = document.querySelector('.top-button');
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
-    mainNavlEl.style.backgroundColor = "inherit";
-    buttonUpEl.classList.remove("hidden");
+    mainNavlEl.style.backgroundColor = 'inherit';
+    buttonUpEl.classList.remove('hidden');
   } else {
-    mainNavlEl.style.backgroundColor = "transparent";
-    buttonUpEl.classList.add("hidden");
+    mainNavlEl.style.backgroundColor = 'transparent';
+    buttonUpEl.classList.add('hidden');
   }
 });
 
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
-// Video player
-const videoPlayer = document.querySelector(".video-player");
-const videoPlayButton = document.querySelector(".video-start-icon");
-const videoCloseButton = document.querySelector(".video-exit-button");
+// Nav menu
+const navMenuButton = document.getElementById('nav-button');
+const navLinks = document.querySelector('.nav-links');
 
-const closeVideoPlayer = () => {
-  videoPlayer.classList.add("hidden");
-};
-
-videoPlayButton.addEventListener("click", () => {
-  videoPlayer.classList.remove("hidden");
+navMenuButton.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  navMenuButton.classList.toggle('open');
 });
 
-videoCloseButton.addEventListener("click", closeVideoPlayer);
+// Video player
+const videoPlayer = document.querySelector('.video-player');
+const videoPlayButton = document.querySelector('.video-start-icon');
+const videoCloseButton = document.querySelector('.video-exit-button');
 
-document.body.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !videoPlayer.classList.contains("hidden"))
+const closeVideoPlayer = () => {
+  videoPlayer.classList.add('hidden');
+};
+
+videoPlayButton.addEventListener('click', () => {
+  videoPlayer.classList.remove('hidden');
+});
+
+videoCloseButton.addEventListener('click', closeVideoPlayer);
+
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !videoPlayer.classList.contains('hidden'))
     closeVideoPlayer();
 });
 
 // Toggle butotns
-document.querySelectorAll(".toggle-button").forEach((current) => {
-  current.addEventListener("click", () => {
-    current.classList.toggle("switch-side");
+document.querySelectorAll('.toggle-button').forEach((current) => {
+  current.addEventListener('click', () => {
+    current.classList.toggle('switch-side');
   });
 });
 
 // Smooth scrolling
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
 
-    if (href === "#") {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    } else if (href.startsWith("#")) {
+    if (href === '#') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else if (href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
 
     // Close mobile navigation
-    if (header.classList.contains("nav-open")) {
-      header.classList.remove("nav-open");
+    if (header.classList.contains('nav-open')) {
+      header.classList.remove('nav-open');
     }
   });
 });
